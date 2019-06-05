8b1e09c83a69a02e77b3a6bc0425b80e9d053a31
Øyvind Raddum Berg <elacin@gmail.com>
2019-06-05 18:20:57 +0200

    Add support for opposite direction

--- facades/react-slinky/src/main/scala/typings/reactLib/ScalableSlinky.scala
+++ facades/react-slinky/src/main/scala/typings/reactLib/ScalableSlinky.scala
@@ -1,15 +1,14 @@
-package hello.world
+package typings.reactLib
 
 import org.scalablytyped.runtime.{Instantiable1, Instantiable2}
-import slinky.core.BuildingComponent
+import slinky.core.{BuildingComponent, KeyAddingStage, KeyAndRefAddingStage, WithAttrs}
+import typings.reactLib.reactMod._
 
+import scala.language.implicitConversions
 import scala.scalajs.js
 
 object ScalableSlinky {
-  import typings.reactLib.reactMod._
-
-  import scala.language.implicitConversions
-
+  /* Support using ScalablyTyped components as slinky ExternalComponents */
   @inline implicit def fromExoticComponent[P <: js.Object](c: ExoticComponent[P]): AwaitProps[P] = new AwaitProps[P](c.asInstanceOf[ComponentType[P]])
   @inline implicit def fromComponentClass[P <: js.Object](c: ComponentClass[P, _]): AwaitProps[P] = new AwaitProps[P](c.asInstanceOf[ComponentType[P]])
   @inline implicit def fromInstantiable1[P <: js.Object](c: Instantiable1[P, ReactElement[_]]): AwaitProps[P] = new AwaitProps[P](c.asInstanceOf[ComponentType[P]])
@@ -24,4 +23,17 @@ object ScalableSlinky {
     @inline def noprops: BuildingComponent[Nothing, js.Object] =
       new BuildingComponent(js.Array(ctor.asInstanceOf[js.Any], new js.Object))
   }
+
+  /* Support using Slinky things when a ScalablyTyped `ReactNode` is expected */
+  @inline implicit def buildExternal[E, R <: js.Object](comp: BuildingComponent[E, R]): ReactNode =
+    BuildingComponent.make[E, R](comp).asInstanceOf[ReactNode]
+
+  @inline implicit def buildWithAttrs[A](withAttrs: WithAttrs[A]): ReactNode =
+    WithAttrs.build(withAttrs).asInstanceOf[ReactNode]
+
+  @inline implicit def buildKeyAddingState(stage: KeyAddingStage): ReactNode =
+    KeyAddingStage.build(stage).asInstanceOf[ReactNode]
+
+  @inline implicit def buildKeyAndRefAddingStage[D](stage: KeyAndRefAddingStage[D]): ReactNode =
+    KeyAndRefAddingStage.build(stage).asInstanceOf[ReactNode]
 }
