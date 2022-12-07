f1fd243ab8592251a8cfe300cd6c881e97f40986
Tsobot <tso@olvind.com>
2022-12-07 07:00:54 +0100

    Converter: dbe791 - 17 new successes, 0 new failures
    New successes:
    express-brute-mongo
    gridfs-stream
    indy-sdk
    koa-mongo
    meteor-astronomy
    meteor-dburles-collection-helpers
    meteor-mdg-validated-method
    meteor-mdg-validation-error
    meteor-persistent-session
    meteor-prime8consulting-oauth2
    meteor-publish-composite
    meteor-roles
    migrate-mongo
    mongodb-queue
    mongorito
    mongration
    node-mongodb-fixtures
    
    New failures:
    
    Old failures
    @aws-sdk/protocol-json-rpc
    @aws-sdk/protocol-query
    @aws-sdk/protocol-rest
    @ckeditor/ckeditor5-utils
    @ember/object
    @esfx/canceltoken
    @ethereumjs/util
    @feathersjs/authentication-oauth2
    @firebase/functions-compat
    @firebase/performance-compat
    @firebase/remote-config-compat
    @fluentui/keyboard-key
    @jupyterlab/extensionmanager
    @jupyterlab/notebook
    @nrwl/workspace
    @pixi-spine/base
    @pixi/events
    @react-md/layout
    @stylable/core
    @theme-ui/css
    @xmpp/jid
    ag-grid
    androiduix
    angular-es
    applicationinsights
    ast-types
    async-eventemitter
    buffer
    carbon-components-react
    clone
    coffeescript
    datastore-level
    dojo
    egg-mock
    es-abstract
    eslint
    espruino
    eth-block-tracker
    execa
    gatsby
    get-intrinsic
    glob-expand
    graphql
    graphql-relay
    gsap
    inputmask
    instagram-private-api
    ipfs-repo
    jointjs
    jsnlog
    keyv
    knex
    kythe
    lodash
    logat
    looks-same
    maptalks
    mdast-util-to-hast
    micro-memoize
    min-document
    mongoose
    msportalfx-test
    multiplexjs
    node-notifier
    notify
    openfin
    passport-auth-token
    passport-fido2-webauthn
    passport-local
    pouchdb-core
    qiniu-js
    ramda
    react-pivottable
    react-three-fiber
    restify-errors
    rx-lite-testing
    simpleddp
    sparql-http-client
    sql.js
    stellar-base
    stream-csv-as-json
    stream-json
    styled-system
    svg.js
    titanium
    typesafe-actions
    v8-compile-cache-lib
    vinyl-fs
    webostvjs
    yup

--- a/angular__core/src/main/scala/typings/angularCore/schematicsUtilsLoadEsmMod.scala
+++ a/angular__core/src/main/scala/typings/angularCore/schematicsUtilsLoadEsmMod.scala
@@ -1,6 +1,5 @@
 package typings.angularCore
 
-import typings.angularCore.anon.TypeofimportedMigrations
 import typings.node.urlMod.URL_
 import org.scalablytyped.runtime.StObject
 import scala.scalajs.js
@@ -12,7 +11,11 @@ object schematicsUtilsLoadEsmMod {
   @js.native
   val ^ : js.Any = js.native
   
-  inline def loadCompilerCliMigrationsModule(): js.Promise[TypeofimportedMigrations] = ^.asInstanceOf[js.Dynamic].applyDynamic("loadCompilerCliMigrationsModule")().asInstanceOf[js.Promise[TypeofimportedMigrations]]
+  inline def loadCompilerCliMigrationsModule(): js.Promise[
+    /* import warning: ResolveTypeQueries.resolve Couldn't resolve typeof imported_migrations */ Any
+  ] = ^.asInstanceOf[js.Dynamic].applyDynamic("loadCompilerCliMigrationsModule")().asInstanceOf[js.Promise[
+    /* import warning: ResolveTypeQueries.resolve Couldn't resolve typeof imported_migrations */ Any
+  ]]
   
   inline def loadEsmModule[T](modulePath: String): js.Promise[T] = ^.asInstanceOf[js.Dynamic].applyDynamic("loadEsmModule")(modulePath.asInstanceOf[js.Any]).asInstanceOf[js.Promise[T]]
   inline def loadEsmModule[T](modulePath: URL_): js.Promise[T] = ^.asInstanceOf[js.Dynamic].applyDynamic("loadEsmModule")(modulePath.asInstanceOf[js.Any]).asInstanceOf[js.Promise[T]]
